40e34009f64e182018ae8cae53f9de1663778f40
Hayden Gomes <hayden@palominolabs.com>
2014-06-16 13:06:43 -0700

    work in progress

--- sites/puppy/templates/home.php
+++ sites/puppy/templates/home.php
@@ -1,86 +1,88 @@
 <div id="masthead">
-    <img src="img/masthead.png">
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+                <span class="masthead-main-line">Request items from around the world, no matter where you live.</span>
 
-    <div class="overlay">
-        Request items from around the world, no matter where you live.
+                <div class="masthead-call-to-action">
+                    I'm looking for:
+                    <div>
+                        <input type="textfield" id="request-item-name"/>
+
+                        <button type="button">Make a Request</button>
+                    </div>
+                </div>
+        </div>
 
         <div class='call-to-action'>
-            I'm looking for:<br>
-            <input type="textfield" id="request-item-name"/>
-            <button type="button">Request an item</button>
+            <div class="row">
+                <div class="col-xs-2 col-xs-offset-1"></div>
+            </div>
+
         </div>
     </div>
 </div>
-<div class="content-body">
-    <h4>Here's how it works:</h4>
 
-    <div class="row">
-        <div class="col-sm-3">
-            <div class="signup-step">
-                <img src="/img/step-icon.png">
-                <b>Describe The Item.</b>
-                Can't find something in your local stores? Tell us what to look for.
+<div class="content-body">
+    <div class="how-it-works">
+        <div class="section-header">Here's how it works:</div>
+
+        <div class="row">
+            <div class="col-xs-4">
+                <div class="signup-step">
+                    <img src="/img/step-icon.png">
+                    <b>Describe The Item.</b>
+                    Can't find something in your local stores? Tell us what to look for.
+                </div>
             </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="signup-step">
-                <img src="/img/step-icon.png">
-                <b>Request Bids.</b>
-                Parcel Puppies submit bids.
+            <div class="col-xs-4">
+                <div class="signup-step">
+                    <img src="/img/step-icon.png">
+                    <b>Get a Parcel Puppy.</b>
+                    You choose a Parcel Puppy who will ship it to you.
+                </div>
             </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="signup-step">
-                <img src="/img/step-icon.png">
-                <b>Get a Parcel Puppy.</b>
-                You choose a Parcel Puppy who will ship it to you.
-            </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="signup-step">
-                <img src="/img/step-icon.png">
-                <b>Pay when your item ships.</b>
-                You are only charged when your item ships. We'll keep you updated with tracking information.
+            <div class="col-xs-4">
+                <div class="signup-step">
+                    <img src="/img/step-icon.png">
+                    <b>Pay when your item ships.</b>
+                    You are only charged when your item ships. We'll keep you updated with tracking information.
+                </div>
             </div>
         </div>
     </div>
 
     <div class="recommendation-row">
         <div class="recommendation-header">
-            People from <a>San Francisco, CA</a> have requested: <a class="rec-filter">Change Location<img
+            People from <a>The United States</a> have requested: <a class="rec-filter">Change Location<img
                     src="img/down-arrow.png" class="down-arrow"></a>
         </div>
 
         <div class="rec-items row">
-            <div class="col-sm-3">
-                <div class="rec-item-img" style="background-image: url('/img/item-img.png')">
-
-                </div>
-                Gifrer Liniment Oléo-Calcaire
+            <div class="col-xs-3">
+                <div class="rec-item-img" style="background-image: url('/img/item-img.png')"> </div>
+                <div class="rec-item-name">Gifrer Liniment Oléo-Calcaire</div>
+                <div class="rec-item-origin"><a>France</a></div>
             </div>
-            <div class="col-sm-3">
-                <div class="rec-item-img" style="background-image: url('/img/item-img.png')">
-
-                </div>
-                Gifrer Liniment Oléo-Calcaire
+            <div class="col-xs-3">
+                <div class="rec-item-img" style="background-image: url('/img/item-img.png')"> </div>
+                <div class="rec-item-name">Gifrer Liniment Oléo-Calcaire</div>
+                <div class="rec-item-origin"><a>France</a></div>
             </div>
-            <div class="col-sm-3">
-                <div class="rec-item-img" style="background-image: url('/img/item-img.png')">
-
-                </div>
-                Gifrer Liniment Oléo-Calcaire
+            <div class="col-xs-3">
+                <div class="rec-item-img" style="background-image: url('/img/item-img.png')"> </div>
+                <div class="rec-item-name">Gifrer Liniment Oléo-Calcaire</div>
+                <div class="rec-item-origin"><a>France</a></div>
             </div>
-            <div class="col-sm-3">
-                <div class="rec-item-img" style="background-image: url('/img/item-img.png')">
-
-                </div>
-                Gifrer Liniment Oléo-Calcaire
+            <div class="col-xs-3">
+                <div class="rec-item-img" style="background-image: url('/img/item-img.png')"> </div>
+                <div class="rec-item-name">Gifrer Liniment Oléo-Calcaire</div>
+                <div class="rec-item-origin"><a>France</a></div>
             </div>
         </div>
 
         <div class="rec-see-more">
             <button type="button">See More</button>
-            <br>Discover Locally-sourced<br>goods from around the world.
+            <br>Discover Locally-sourced goods<br>from around the world.
         </div>
     </div>
 </div>
